51b138cf247623afaa1c9da205a1b28747735e2d
Xwilder <50119735+Xwilder@users.noreply.github.com>
2019-09-27 22:31:12 +0400

    Update header.js

--- source/js/header.js
+++ source/js/header.js
@@ -2,6 +2,8 @@ var mainNav = document.querySelector(".main-nav__list");
 var navHead = document.querySelector(".page-header__nav-header");
 var menubtn = document.querySelector(".menu-btn");
 
+var slideIndex = 1;
+
 
 window.onload = function (evt) {
   evt.preventDefault();
@@ -15,3 +17,35 @@ menubtn.addEventListener("click", function toggle (evt) {
   navHead.classList.toggle("nav-hidden");
   mainNav.classList.toggle("nav-close");
 });
+
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("reviews__item");
+  var toggle = document.getElementsByClassName("slider__toggle");
+
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i=0; i < slides.length; i++){
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < toggle.length; i++){
+    toggle[i].className = toggle[i].className.replace("active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+
+  toggle[slideIndex-1].className+= " active";
+}
